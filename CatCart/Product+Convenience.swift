5f8794dc7053d6624b8522d9c8c3de3713aa6207
Crystal Knight <crystalknightcodes@gmail.com>
2020-06-22 23:30:26 -0400

    Added and configured Product+Convenience

---
+++ CatCart/Product+Convenience.swift
@@ -0,0 +1,46 @@
+//
+//  Product+Convenience.swift
+//  CatCart
+//
+//  Created by Christy Hicks on 6/22/20.
+//  Copyright © 2020 CatCartCo. All rights reserved.
+//
+
+import Foundation
+import CoreData
+
+extension Product {
+    
+    convenience init(id: UUID = UUID(),
+                     name: String,
+                     price: Double,
+                     type: String,
+                     size: String?,
+                     color: String?,
+                     imageURL: String?,
+                     context: NSManagedObjectContext = CoreDataStack.shared.mainContext) {
+        
+        self.init(context: context)
+        
+        self.id = id
+        self.name = name
+        self.price = price
+        self.type = type
+        self.size = size
+        self.color = color
+        self.imageURL = imageURL
+    }
+    
+    convenience init?(productRepresentation: ProductRepresentation, context: NSManagedObjectContext = CoreDataStack.shared.mainContext) {
+        
+        guard let id = productRepresentation.id,
+            let name = productRepresentation.name,
+            let price = productRepresentation.price,
+            let type = productRepresentation.type,
+            let size = productRepresentation.size,
+            let color = productRepresentation.color,
+            let imageURL = productRepresentation.imageURL else { return nil }
+        
+        self.init(id: id, name: name, price: price, type: type, size: size, color: color, imageURL: imageURL, context: context)
+    }
+}
