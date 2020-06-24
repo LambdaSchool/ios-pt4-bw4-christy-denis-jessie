0fdad3a8a98c83b47adbfa1a8da248b50a3ee64b
Crystal Knight <crystalknightcodes@gmail.com>
2020-06-24 14:46:56 -0400

    Configured Cats Detail View Controller

--- CatCart/CatDetailViewController.swift
+++ CatCart/CatDetailViewController.swift
@@ -9,22 +9,45 @@
 import UIKit
 
 class CatDetailViewController: UIViewController {
-
+    
+    // MARK: - Outlets
+    @IBOutlet var catImageView: UIImageView!
+    @IBOutlet var nameLabel: UILabel!
+    @IBOutlet var priceLabel: UILabel!
+    @IBOutlet var yearsLabel: UILabel!
+    @IBOutlet var monthsLabel: UILabel!
+    
+    // MARK: - Properties
+    var cat: Cat?
+    
+    // MARK: - View
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        
+        updateViews()
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    func updateViews() {
+        guard let cat = cat else { return }
+        priceLabel.text = "$\(cat.price)"
+        nameLabel.text = cat.name
+        yearsLabel.text = "\(cat.years)"
+        monthsLabel.text = "\(cat.months)"
+        getImage()
     }
-    */
-
+    
+    func getImage() {
+        guard let cat = cat else { return }
+        if let catImageURLString = cat.imageURL {
+            guard let catImageURL = URL(string: catImageURLString) else { return }
+            DispatchQueue.global().async {
+                guard let imageData = try? Data(contentsOf: catImageURL) else { return }
+                
+                let catImage = UIImage(data: imageData)
+                DispatchQueue.main.async {
+                    self.catImageView.image = catImage
+                }
+            }
+        }
+    }
 }
