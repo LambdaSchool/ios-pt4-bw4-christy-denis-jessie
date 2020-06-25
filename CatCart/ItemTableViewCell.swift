29812722ea76b2a63ad399bc5fc22072dfe33190
Jessie <jessie.griffin@icloud.com>
2020-06-24 23:02:30 -0400

    Adds ShoppingCart Controller and ItemtableViewCell

---
+++ CatCart/ItemTableViewCell.swift
@@ -0,0 +1,16 @@
+//
+//  ItemTableViewCell.swift
+//  CatCart
+//
+//  Created by Jessie Ann Griffin on 6/24/20.
+//  Copyright © 2020 CatCartCo. All rights reserved.
+//
+
+import UIKit
+
+class ItemTableViewCell: UITableViewCell {
+    
+    @IBOutlet weak var nameLabel: UILabel!
+    @IBOutlet weak var priceLabel: UILabel!
+
+}
---
+++ CatCart/ShoppingCartController.swift
@@ -0,0 +1,34 @@
+//
+//  ShoppingCartController.swift
+//  CatCart
+//
+//  Created by Jessie Ann Griffin on 6/24/20.
+//  Copyright © 2020 CatCartCo. All rights reserved.
+//
+
+import Foundation
+
+class ShoppingCartController {
+
+    var cat: Cat?
+    var product: Product?
+
+    var itemNames: [String] = []
+    var itemPrices: [Double] = []
+
+    var userController: UserController?
+    var currentUser: User?
+
+    func addItem(itemName: String, itemPrice: Double) {
+        itemNames.append(itemName)
+        itemPrices.append(itemPrice)
+    }
+
+    func updateCart() { // POST
+
+    }
+
+    func deleteFromCart() { // DELETE
+
+    }
+}
