7980ea483858153afb3aba5fe42e5ae87982631e
Denis Cedeno <denis.cedeno@gmail.com>
2020-06-25 08:29:33 -0400

    created mock coordinates for cats around apple headquarters

--- CatCart/CatsNearbyMapViewController.swift
+++ CatCart/CatsNearbyMapViewController.swift
@@ -15,7 +15,22 @@ extension String {
 }
 
 class MapViewController: UIViewController {
-    
+    // MARK: - Mock Data
+
+       let cat1 = Cat(name: "Mysty", price: 14.99, latitude: 37.333768, longitude: -122.033954, years: 0, months: 2, imageURL: "https://imagizer.imageshack.com/img922/2816/kDUKLs.jpg")
+       let cat2 = Cat(name: "Arya", price: 2.49, latitude: 37.337830, longitude: -122.033493, years: 0, months: 6, imageURL: "https://imagizer.imageshack.com/img923/6269/C6kuyK.jpg")
+       let cat3 = Cat(name: "Eclipse", price: 74.99, latitude: 37.370036, longitude: -122.032052, years: 1, months: 3, imageURL: "https://imagizer.imageshack.com/img923/762/dhCFUn.jpg")
+       let cat4 = Cat(name: "Soledad", price: 0.99, latitude: 37.325935, longitude: -122.020652, years: 5, months: 3, imageURL: "https://imagizer.imageshack.com/img922/7587/3rbsUm.jpg")
+       let cat5 = Cat(name: "Thunder", price: 49.99, latitude: 37.328590, longitude: -122.037386, years: 0, months: 7, imageURL: "https://imagizer.imageshack.com/img923/1834/py9HKC.jpg")
+       
+       func addMockData() {
+           cats.append(cat1)
+           cats.append(cat2)
+           cats.append(cat3)
+           cats.append(cat4)
+           cats.append(cat5)
+       }
+       
     var cats: [Cat] = []
     
     @IBOutlet var mapView: MKMapView!
@@ -25,6 +40,7 @@ class MapViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        addMockData()
         locationManager?.requestWhenInUseAuthorization()
         
         userTrackingButton = MKUserTrackingButton(mapView: mapView)
