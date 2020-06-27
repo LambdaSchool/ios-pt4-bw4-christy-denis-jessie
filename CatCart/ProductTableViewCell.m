60a3efc5acd57923ee8c3f539fa370a83ca979e7
Crystal Knight <crystalknightcodes@gmail.com>
2020-06-26 23:07:11 -0400

    Product images now display in Product Table View Controller
    (cherry picked from commit 7b26fb2747505345f1a62569dcb8a7361ebb87a7)

--- CatCart/ProductTableViewCell.m
+++ CatCart/ProductTableViewCell.m
@@ -18,12 +18,31 @@
 - (void)setProduct:(Product *)product {
     _productNameLabel.text = product.name;
     _productPriceLabel.text=[NSString stringWithFormat:@"$%.2lf", product.price];
+    [self loadImage:product.imageURL];
 }
 
 - (void)setSelected:(BOOL)selected animated:(BOOL)animated {
     [super setSelected:selected animated:animated];
 
     // Configure the view for the selected state
+}
+
+- (void)loadImage:(NSString *)productImageURLString {
+    NSURL *productImageURL = [NSURL URLWithString:productImageURLString];
+    NSOperationQueue *queue = [NSOperationQueue new];
+    NSInvocationOperation *operation = [[NSInvocationOperation alloc] initWithTarget:self selector:@selector(requestRemoteImage:) object:productImageURL];
+    [queue addOperation:operation];
+}
+
+- (void)requestRemoteImage:(NSURL *)productImageURL {
+    NSData *productImageData = [[NSData alloc] initWithContentsOfURL:productImageURL];
+    UIImage *productImage = [[UIImage alloc] initWithData:productImageData];
+    
+    [self performSelectorOnMainThread:@selector(placeImageInUI:) withObject:productImage waitUntilDone:YES];
+}
+
+- (void)placeImageInUI:(UIImage *)productImage {
+    [_productImageView setImage:productImage];
 }
 
 @end
