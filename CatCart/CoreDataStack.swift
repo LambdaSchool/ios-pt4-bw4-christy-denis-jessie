6253b9cb5ad39a841abff368d007a14deb70e4ed
Denis Cedeno <denis.cedeno@gmail.com>
2020-06-27 15:30:15 -0400

    added save to coreData stack

--- CatCart/CoreDataStack.swift
+++ CatCart/CoreDataStack.swift
@@ -14,15 +14,34 @@ class CoreDataStack {
     
     lazy var container: NSPersistentContainer = {
        let newContainer = NSPersistentContainer(name: "CatCoreData")
+        
         newContainer.loadPersistentStores { (_, error) in
             guard error == nil else {
                 fatalError("Failed to load persistent stores: \(error!)")
             }
         }
+        newContainer.viewContext.automaticallyMergesChangesFromParent = true
         return newContainer
     }()
     
     var mainContext: NSManagedObjectContext {
         return container.viewContext
     }
+    
+    
+    func save(context: NSManagedObjectContext = CoreDataStack.shared.mainContext) throws {
+        var saveError: Error?
+        
+        context.performAndWait {
+            do {
+                try context.save()
+            } catch {
+                saveError = error
+                context.reset()
+            }
+        }
+        
+        if let saveError = saveError { throw saveError }
+    }
+    
 }
