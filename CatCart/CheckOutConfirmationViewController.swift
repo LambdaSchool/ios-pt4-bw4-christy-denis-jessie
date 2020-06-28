5d8d82a208408a5022c7cb699e842f87fe03ff82
Denis Cedeno <denis.cedeno@gmail.com>
2020-06-28 08:17:20 -0400

    created labels to display checkout cart information, created place order button, added alert view that take you to the category view

--- CatCart/CheckOutConfirmationViewController.swift
+++ CatCart/CheckOutConfirmationViewController.swift
@@ -9,30 +9,108 @@
 import UIKit
 
 class CheckOutConfirmationViewController: UIViewController {
-
+    
     
     var currentUser: User?
     var currentUserCreditCard: CreditCard?
     var currentUserShippingAddress: ShippingAdress?
     var cartController = ShoppingCartController.shared
-
+    
+    @IBOutlet weak var itemCountLabel: UILabel!
+    
+    @IBOutlet weak var totalLabel: UILabel!
+    
+    @IBOutlet weak var firstNameLabel: UILabel!
+    
+    @IBOutlet weak var lastNameLabel: UILabel!
+    
+    @IBOutlet weak var streetAddress: UILabel!
+    
+    @IBOutlet weak var cityLabel: UILabel!
+    
+    @IBOutlet weak var stateLabel: UILabel!
+    
+    @IBOutlet weak var zipCodeLabel: UILabel!
+    
+    @IBOutlet weak var cardEndingInLabel: UILabel!
+    
+    
+    @IBAction func placeOrderButton(_ sender: Any) {
+        
+        DispatchQueue.main.async {
+            let alertController = UIAlertController(
+                title: "Thank You!!",
+                message: "Your Order has been placed",
+                preferredStyle: .alert)
+            let alertAction = UIAlertAction(
+                title: "OK",
+                style: UIAlertAction.Style.default,
+                handler: { action in
+                    self.navigationController?.popToRootViewController(animated: true)
+            } )
+            alertController.addAction(alertAction)
+            self.present(alertController, animated: true)
+        }
+        
+    }
+    
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        if currentUser == currentUser {
-        print("First Name: \(String(describing: currentUser?.firstName)), CVV Code: \(String(describing: currentUserCreditCard?.creditCardCVV)), Shipping Address: \(String(describing: currentUserShippingAddress?.shippingAddress)), number of items: \(String(describing: cartController.itemNames.count))")
-        }
+        
+        let currencyFormatter = NumberFormatter()
+        currencyFormatter.usesGroupingSeparator = true
+        currencyFormatter.numberStyle = .currency
+        currencyFormatter.locale = Locale.current
+        
+        updateViews(formatter: currencyFormatter)
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    
+    func updateViews(formatter: NumberFormatter) {
+        
+        
+        var subtotal = 0.0
+        for price in cartController.itemPrices {
+            subtotal += price
+        }
+        if let total = formatter.string(from: NSNumber(value: subtotal)) {
+            totalLabel.text = "\(total)"
+        }
+        
+        itemCountLabel.text = "\(cartController.itemNames.count)"
+        
+        
+        if currentUser == currentUser {
+            firstNameLabel.text = currentUser?.firstName
+            lastNameLabel.text = currentUser?.lastName
+        }
+        
+        streetAddress.text = currentUserShippingAddress?.shippingAddress
+        cityLabel.text = currentUserShippingAddress?.shippingCity
+        stateLabel.text = currentUserShippingAddress?.shippingState
+        
+        
+        zipCodeLabel.text = "\(currentUserShippingAddress?.shippingZip ?? 0)"
+        
+        let ccNumber = "\(String(describing: currentUserCreditCard?.creditCardNumber ?? 1111))"
+        let last4 = ccNumber.suffix(4)
+        
+        cardEndingInLabel.text = String(last4)
+        
     }
-    */
-
+    
+    /*
+     // MARK: - Navigation
+     
+     // In a storyboard-based application, you will often want to do a little preparation before navigation
+     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+     // Get the new view controller using segue.destination.
+     // Pass the selected object to the new view controller.
+     }
+     */
+    
 }
